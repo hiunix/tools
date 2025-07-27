91c6f8886d65734cf8e41a177cb4c66c8badbe14
hiunix <hellolinux@qq.com>
2025-07-27 20:00:20 +0800

    linux install script

---
+++ linux/install.sh
@@ -0,0 +1,9 @@
+sudoapt install build-essential -y
+sudo apt install vim -y
+sudo apt install fonts-noto-cjk fonts-noto-color-emoji fonts-wqy-zenhei fonts-wqy-microhei -y
+sudo apt install ttf-mscorefonts-installer -y
+sudo apt install libx11-xcb-dev -y
+sudo apt install libxrender-dev -y
+sudo apt install libxkbcommon-x11-dev -y
+sudo apt install cmake -y
+sudo apt install libgl1-mesa-dev -y
